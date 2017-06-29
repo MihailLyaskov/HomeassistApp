9c290e9e7967d26357d407e51b50dba69347fa8b
Mihail Lyaskov <m.lyaskov@abv.bg>
2017-06-29 23:32:00 +0300

    added install script for armbian on Olinuxino

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Install nodejs , npm and pm2
+sudo apt-get install -y curl
+sudo apt-get install -y build-essential
+curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo npm install npm@latest -g
+sudo npm install pm2@latest -g
+# Install MongoDB
+sudo apt-get install -y mongodb
+
+curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+source /etc/os-release
+test $VERSION_ID = "8" && echo "deb https://repos.influxdata.com/debian jessie stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+
+sudo apt-get update && sudo apt-get install -y influxdb
+sudo service influxdb start
+
+sudo apt-get install -y python-pip
+sudo pip install influxdb
+
+
+npm install
+
+cd services/database
+chmod +x influxDriver.py
